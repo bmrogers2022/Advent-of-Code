361dbc584454fc26ec395cc38f644ec442895fd6
bmrogers2022 <bmrogers@ucsd.edu>
2023-12-10 00:45:05 -0800

    Day 9 done
    late for finals

---
+++ day_nine/README.md
@@ -0,0 +1,15 @@
+# Day 9 Notes
+
+Late solution today because I had a final
+
+# Part One
+###### Personal difficulty rating = 2
+
+### Solution logic
+Another easily brute forceable problem. Just gonna start with that and hope that it also works for part two.
+
+# Part Two
+###### Personal difficulty rating = 2
+
+### Solution logic
+Brute forcing should work too (literally the exact same code, just reverse the list). What a chill and easy day for my final.
---
+++ day_nine/part_one.py
@@ -0,0 +1,37 @@
+# variables to make changing between testing and running easy
+problem_file = "data.txt"
+tester_file = "test.txt"
+
+# put file lines into data
+with open(problem_file) as file:
+    data = file.readlines()
+    data = [[int(y) for y in x.split()] for x in data]
+
+answer =  0
+
+# find values of each group
+for group in data:
+    history_value = 0
+    curr_group = group
+    not_equal = True
+    while not_equal:
+        not_equal = False
+        diff_value = curr_group[0] - curr_group[1]
+        new_group = [diff_value]
+        for i in range(2, len(curr_group)):
+            temp = diff_value
+            diff_value = curr_group[i]-curr_group[i-1]
+            if temp != diff_value:
+                not_equal = True
+            new_group.append(diff_value)
+        history_value += curr_group[-1]
+        curr_group = new_group
+    print(history_value)
+    answer += history_value
+
+print(answer)
+
+"""
+Tester correctly outputs 114
+Problem correctly outputs 1819125966
+"""
