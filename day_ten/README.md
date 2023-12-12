a26b9e0187f532fdf82ae43f649458c5309407b4
bmrogers2022 <bmrogers@ucsd.edu>
2023-12-11 16:43:38 -0800

    Day 10 Part 1
    Last solution upload

--- day_ten/README.md
+++ day_ten/README.md
@@ -1,18 +1,10 @@
 # Day 10 Notes
 
-Another late solution due to conflict with finals, should still be up at least the day after the problem was published though
+No motivation to do part 2 of this problem at all unfortunately, think I'll be calling it here. I just know it would be another 8+ hour problem, especially with how sloppy my solution to part one was. We had a good run.
 
 # Part One
-###### Personal difficulty rating = (TODO)
+###### Personal difficulty rating = 4
 
 ### Solution logic
-Seems to be a simple BFS solution, except we want the longest distance instead of the shortest. I admittedly haven't done one of these in awhile, so it should be good practice.
-
-# Part Two
-###### Personal difficulty rating = (TODO)
-
-### Solution logic
-(TODO)
-
-### Exceptions to logic
-(TODO)
+Seems to be a simple BFS solution, except we want the longest distance instead of the shortest. I admittedly haven't done one of these in awhile, so it should be good practice.  
+Updated: On second thought, I can just traverse the entire path and then divide by two, so I'll do that because its slightly faster.
--- day_ten/part_one.py
+++ day_ten/part_one.py
@@ -1,18 +1,20 @@
 # variables to make changing between testing and running easy
 problem_file = "data.txt"
-tester_file = "test.txt"
+tester_file = "test_two.txt"
 
 # put file lines into data
-with open(tester_file) as file:
+with open(problem_file) as file:
     data = [x.strip() for x in file.readlines()] # removes \n
 
 # imagine 2d array as a grid, these are the directions
 d = {"N": (-1, 0), "S": (1, 0), "E": (0, 1), "W": (0, -1)}
 pipes = {"|": (d["N"], d["S"]), "-": (d["E"], d["W"]), "L": (d["N"], d["E"]),
-         "J": (d["N"], ["W"]), "7": (d["S"], d["W"]), "F": (d["S"], d["E"])}
+         "J": (d["N"], d["W"]), "7": (d["S"], d["W"]), "F": (d["S"], d["E"]),
+         ".": (), "S": ()}
 max_x = len(data[0]) - 1
 max_y = len(data) - 1
 visited = set() # don't double count
+max_distance = -1
 queue = []
 
 # check for index error
@@ -26,10 +28,9 @@ def find_s(map):
             if map[i][j] == "S":
                 return [i, j]
 
-# input 2 lists of coords and a pipe, return next direction or False     
+# input 2 lists of coords, return next direction or False     
 def in_pipe(x, y):
     pipe = data[y[0]][y[1]]
-    print(pipe)
     for i in pipes[pipe]:
         if [y[0] + i[0], y[1] + i[1]] == x:
             for j in pipes[pipe]:
@@ -37,21 +38,34 @@ def in_pipe(x, y):
                     return j # the other direction
     return False
 
-print(data)
-
 # Start traversing array
 l = find_s(data) # location
 symbol = data[l[0]][l[1]]
-
-print(symbol)
-
-print(in_pipe([1,1], [2,1]))
 
 # hard part is starting, after that it just follows a simple direction
 for i in d.values():
     new_pipe = [l[0]+i[0], l[1]+i[1]]
     adjacent = in_pipe(l, new_pipe)
     if adjacent:
-        queue.append([new_pipe, adjacent, 0])
+        queue.append([new_pipe, adjacent, 1])
 
-print(queue)
+while queue:
+    # get all data from front of queue
+    current = queue[0]
+    loc = current[0]
+    dir = current[1]
+    moves = current[2]
+    new_pipe = [loc[0]+dir[0], loc[1]+dir[1]]
+    pipe_str = str(new_pipe[0]) + str(new_pipe[1])
+    next_dir = in_pipe(loc, new_pipe)
+    if next_dir:
+        queue.append([new_pipe, next_dir, moves+1])
+    queue.pop(0)
+
+print(int((moves+1)/2))
+
+"""
+Test 1 correctly outputs 4
+Test 2 correctly outputs 8
+Problem correctly outputs 6754
+"""
